5918c2adfb2375f5e20230f531c52f02e377290c
510_1703_VS\1 <julia_yetskalo@ukr.net>
2017-10-25 09:54:27 -0700

    start

---
+++ hw1/hw1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor concol1 = Console.BackgroundColor;
+            Console.BackgroundColor = ConsoleColor.Green;
+            ConsoleColor concol = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello World, Yulia!");
+            Console.ReadLine();
+        }
+    }
+}
